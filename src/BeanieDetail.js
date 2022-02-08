dafa12c13f1c664d475b815f313879b9f0380130
TrevorRezac <trev.rezac@gmail.com>
2022-02-07 18:01:14 -0800

    clicking beanieBaby sends to beaniepedia

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -8,23 +8,25 @@ export default function BeanieDetail() {
   const [beanieBaby, setBeanieBaby] = useState({});
   // you'll need to get the route param using the appropriate react router hook
   const params = useParams();
-  // console.log('||', beanieBaby);
-
+  
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
     async function fetchData() {
       const beanieBaby = await getSingleBeanie(params.id);
-
+      
       setBeanieBaby(beanieBaby);
     }
-
+    
     fetchData();
-
+    
   }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
-
+  
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
+    window.location.href = beanieBaby.link;
   }
+  
+  // console.log('||', beanieBaby);
 
   return (
     <>
