401fc97dcde1d2318a06d4d9b9f8a7454c8c894e
TrevorRezac <trev.rezac@gmail.com>
2022-02-07 16:55:05 -0800

    beanie babe details page renders correctly

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,15 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
+  const [beanieBaby, setBeanieBaby] = useState({});
   // you'll need to get the route param using the appropriate react router hook
+  const params = useParams();
+  console.log('||', beanieBaby);
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
+    async function fetchData() {
+      const beanieBaby = await getSingleBeanie(params.id);
 
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+      setBeanieBaby(beanieBaby);
+    }
+
+    fetchData();
+
+  }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
 
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
@@ -20,7 +31,7 @@ export default function BeanieDetail() {
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
       
       <Link to='/'>Home</Link>
-      {/* <div className='beanie-detail' onClick={handleBeanieClick}>
+      <div className='beanie-detail' onClick={handleBeanieClick}>
         <div className='beanie-data'>
           <p>{beanieBaby.animal}</p>
           <p>{beanieBaby.title}</p>
@@ -38,7 +49,7 @@ export default function BeanieDetail() {
           <p>Swing Tag Generation: {beanieBaby.swingTagGeneration}</p>
           <p>Tush Tag Generation: {beanieBaby.tushTagGeneration}</p>
         </div>
-      </div>   */}
+      </div>  
     </>
   );
 }
