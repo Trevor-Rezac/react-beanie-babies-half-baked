70f6f0f46d64abc93c2f56dc75b39653d0ede810
TrevorRezac <trev.rezac@gmail.com>
2022-02-07 16:03:09 -0800

    minor styling to beanie list

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default function BeanieBaby({ beanieBaby }) {
   return (
+        // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
+    // it should also render the beanie baby's image and show the beanie baby's name
     <Link to={`/beanie-baby-detail/${beanieBaby.id}`}>
       <div className='beanie-baby'>
         {beanieBaby.title}
@@ -9,8 +11,6 @@ export default function BeanieBaby({ beanieBaby }) {
         <img className='beanie-img' src={beanieBaby.image} />
       </div>
     </Link>
-    // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
-    // it should also render the beanie baby's image and show the beanie baby's name
-    // null
+
   );
 }
